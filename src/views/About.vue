ebb29d998a3aefca142b14fa29ff149567fd5cf5
Nutchy <nutchy@loomx.io>
2019-12-28 09:01:29 +0700

    initial project

---
+++ src/views/About.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="about">
+    <h1>This is an about page</h1>
+    <b-modal id="modal-1" title="BootstrapVue">
+      <p class="my-4">Hello from modal!</p>
+    </b-modal>
+  </div>
+</template>
+
+<script>
+import { Vue, Component } from 'vue-property-decorator'
+
+@Component({})
+export default class About extends Vue {
+
+  mounted() {
+    this.$root.$emit('bv::show::modal', 'modal-1')
+  }
+}
+</script>
