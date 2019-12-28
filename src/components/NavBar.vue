b3624613650118471c059be9616dbad64f1b9f3d
Nutchy <nutchy@loomx.io>
2019-12-28 14:28:13 +0700

    update logo

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,18 +1,23 @@
 <template>
     <b-navbar toggleable="lg" v-if="$route.name !== 'landing'">
-    <b-navbar-brand href="/home">
-        <img src="../assets/logo.svg" class="logo" />
-    </b-navbar-brand>
+      <b-container>
+        <b-navbar-brand href="/home">
+            <span>
+              <img src="../assets/logo.svg" class="logo" />
+              </span>
+        </b-navbar-brand>
 
-    <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+        <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
 
-    <b-collapse id="nav-collapse" is-nav>
-      <!-- Right aligned nav items -->
-      <b-navbar-nav class="ml-auto">
-        <b-nav-item href="/questionaire" :class="{active: $route.name === 'questionaire' }">Questionaire</b-nav-item>
-        <b-nav-item href="matching"  :class="{active: $route.name === 'matching' }">List of something</b-nav-item>
-      </b-navbar-nav>
-    </b-collapse>
+        <b-collapse id="nav-collapse" is-nav>
+          <!-- Right aligned nav items -->
+          <b-navbar-nav class="ml-auto">
+            <b-nav-item href="/questionaire" :class="{active: $route.name === 'questionaire' }">Questionaire</b-nav-item>
+            <b-nav-item href="matching"  :class="{active: $route.name === 'matching' }">List of something</b-nav-item>
+          </b-navbar-nav>
+        </b-collapse>
+
+      </b-container>
   </b-navbar>
 </template>
 <script>
@@ -23,7 +28,8 @@
     background: #fafafa;
 }
 .logo {
-    height: 100%;
+    height: 32px;
+    width: auto;
 }
 
 </style>
