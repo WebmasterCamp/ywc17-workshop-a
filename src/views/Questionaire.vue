9aeb442981c4dc644c8c8a76fa08248d4ace0860
Nutchy <nutchy@loomx.io>
2019-12-28 14:21:21 +0700

    add final questionaire

--- src/views/Questionaire.vue
+++ src/views/Questionaire.vue
@@ -4,31 +4,26 @@
             <h2>แบบทดสอบ</h2>
             <div class="my-5">Some logo</div>
             <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corrupti, tempora doloremque omnis et similique quidem minima voluptatem commodi fugit, eveniet natus facilis quas nostrum voluptate dolore. Error adipisci totam dignissimos!</p>
-             <b-row class="my-4" align-h="center">
-               <b-col cols="2" class="text-center">
-                    <b-button pill variant="info" size="lg" block @click="addStep(-1)"><i class="fas fa-arrow-left mr-2"></i>กลับ</b-button>
-               </b-col>
-               <b-col cols="2" class="text-center">
-                    <b-button pill variant="secondary" size="lg" block @click="addStep(1)">ต่อไป <i class="fas fa-arrow-right"></i></b-button>
-               </b-col>
-           </b-row>
         </div>
        <div v-for="(q, i) in loveQuestions" :key="i" class="mb-5">
-           <h4 class="d-block mb-4">{{q.question}}</h4>
-           <b-row  align-h="center" v-if="currentQuestionNo === i + 1">
-               <b-col cols="5" v-for="(a, j) in q.answers" :key="`a${i}${j}`">
-                   <div class="choice-btn p-3 mt-3" @click="onSelectChoice(i, j)">{{a.label}}</div>
-               </b-col>
-           </b-row>
-           <b-row class="my-4" align-h="center">
+           <div  v-if="i == currentQuestionNo">
+                <h4 class="d-block mb-4">{{q.question}}</h4>
+                <b-row  align-h="center">
+                    <b-col cols="5" v-for="(a, j) in q.answers" :key="`a${i}${j}`">
+                        <div class="choice-btn p-3 mt-3" @click="onSelectChoice(i, j)">{{a.label}}</div>
+                    </b-col>
+                </b-row>
+           </div>
+       </div>
+       <b-row class="my-4" align-h="center">
                <b-col cols="2" class="text-center">
                     <b-button pill variant="info" size="lg" block @click="addStep(-1)"><i class="fas fa-arrow-left mr-2"></i>กลับ</b-button>
                </b-col>
                <b-col cols="2" class="text-center">
-                    <b-button pill variant="secondary" size="lg" block @click="addStep(1)" :disabled="currentSelectedChoice === null">ต่อไป <i class="fas fa-arrow-right"></i></b-button>
+                    <b-button pill variant="secondary" size="lg" block @click="addStep(1)" >ต่อไป <i class="fas fa-arrow-right"></i></b-button>
                </b-col>
            </b-row>
-       </div>
+           {{totalQuestions}} {{currentQuestionNo}}
     </b-container>
 </template>
 
@@ -36,26 +31,45 @@
 import Vue from 'vue'
 import { Component } from 'vue-property-decorator'
 import Questionaire from '@/mock_data/questionaire.json'
+import {isLoggedIn} from '../utils'
 
 @Component({
 
 })
 export default class QuestionairePage extends Vue {
 
-    currentQuestionNo = 0
+    currentQuestionNo = -1
     currentSelectedChoice = null
     userAnswer = []
 
     get loveQuestions() {
         return Questionaire.love
     }
+
+    get totalQuestions() {
+        return Questionaire.love.length
+    }
+
     mounted() {
         console.log(Questionaire)
     }
 
     addStep(step) {
         this.currentQuestionNo += step
+
+        if (this.totalQuestions === this.currentQuestionNo) {
+           if (isLoggedIn()) {
+               this.showResult()
+            } else {
+                this.$root.$emit('bv::show::modal', 'auth-modal')
+            }
+
+        }
         this.currentSelectedChoice = null
+    }
+
+    showResult() {
+        this.$router.push({ path: '/result'})
     }
 
     onSelectChoice(questionNo, ansIdx) {
@@ -71,5 +85,6 @@ export default class QuestionairePage extends Vue {
     background: #fff;
     border-radius: 4px;
     cursor: pointer;
+    min-height: 80px;
 }
 </style>
