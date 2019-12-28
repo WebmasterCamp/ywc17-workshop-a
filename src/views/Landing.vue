f3785ea7077fffe94c020cdecb6691054db19853
WP <doliolarzz@hotmail.com>
2019-12-28 15:45:59 +0700

    saf

--- src/views/Landing.vue
+++ src/views/Landing.vue
@@ -1,33 +1,66 @@
 <template>
-    <b-container>
-        <section>
-            <h1 class="my-4 text-center">คุณคือใคร ?</h1>
-            <b-row align-h="center" style="min-height: 50vh;">
-                <b-col cols="5">
-                    <div class="card text-center d-flex h-100 justify-content-center" @click="onSelectActor('taker')">Taker</div>
-                </b-col>
-                <b-col cols="5"><div class="card text-center d-flex h-100 justify-content-center" @click="onSelectActor('giver')">Giver</div></b-col>
-            </b-row>
-        </section>
-    </b-container>
+  <div>
+    <section class="taker-header">
+      <b-container fluid class="h-100">
+        <b-row class="align-items-center h-100">
+          <b-col class="text-center">
+            <b-container>
+            <img src="@/assets/logo-white.svg" class="tlogo" />
+              <section>
+                <h1 class="my-4 text-center text-light">Community ที่เข้าถึงจิตด้วยความเข้าใจ</h1>
+                <b-row align-h="center" style="min-height: 50vh;">
+                  <b-col cols="5">
+                    <div
+                      class="card text-center d-flex h-100 justify-content-center"
+                      @click="onSelectActor('taker')"
+                    >Taker</div>
+                  </b-col>
+                  <b-col cols="5">
+                    <div
+                      class="card text-center d-flex h-100 justify-content-center"
+                      @click="onSelectActor('giver')"
+                    >Giver</div>
+                  </b-col>
+                </b-row>
+              </section>
+            </b-container>
+          </b-col>
+        </b-row>
+      </b-container>
+    </section>
+    <img class="hb w-100" src="@/assets/takerHeader/bottom.svg" />
+  </div>
 </template>
 
 <script>
 export default {
-    methods: {
-        onSelectActor(type) {
-            if (type === 'taker') {
-                this.$router.push({ path: '/taker'})
-            } else {
-                // .. todo
-            }
-        }
+  methods: {
+    onSelectActor(type) {
+      if (type === 'taker') {
+        this.$router.push({ path: '/taker' })
+      } else {
+        // .. todo
+      }
     }
+  }
 }
 </script>
 
 <style scoped>
 .card-select {
-    min-height: 50vh;
+  min-height: 50vh;
+}
+.taker-header {
+  width: 100%;
+  height: 70vh;
+  background-color: #ed0a36;
+  overflow: hidden;
+  background-image: url('../assets/takerHeader/right.svg'), url('../assets/takerHeader/left.svg');
+  background-position: right -50px bottom 0px, -50px bottom;
+  background-repeat: no-repeat, no-repeat;
+  background-size: 25vh, 25vh;
+}
+.tlogo {
+    width: 30%;
 }
 </style>
--- src/views/Taker.vue
+++ src/views/Taker.vue
@@ -3,15 +3,16 @@
     <section class="taker-header">
         <b-container fluid class="h-100">
           <b-row class="align-items-center h-100">
-            <b-col>
-              <h2 class="text-center h-100">บลาๆๆๆๆ</h2>
+            <b-col class="text-center">
+              <h2 class="text-center h-100 ht">บลาๆๆๆๆ</h2>
+              <b-button @click="moveTo('.first-content')" pill variant="secondary mt-5 hbd"><i class="fa fa-arrow-down" aria-hidden="true"></i></b-button>
             </b-col>
           </b-row>
         </b-container>
     </section>
     <img class="hb w-100" src="@/assets/takerHeader/bottom.svg" />
     <section v-for="(d, i) in headerData" :key="'body_'+i">
-      <b-container class="takerChoice m-5">
+      <b-container class="takerChoice m-5 first-content">
         <b-row>
           <b-col cols="8" v-if="i%2==1">
             <b-row class="d-flex justify-content-end">
@@ -71,6 +72,18 @@
   width: 70%;
   height: 70%;
 }
+.ht {
+  color: #FFF;
+  margin: auto;
+}
+.hb {
+  width: 100%;
+  z-index: 99999;
+}
+.hbd {
+  color: white;
+  background-color: white;
+}
 </style>
 
 <script>
@@ -82,6 +95,14 @@ export default {
   components: {},
   data: () => ({
     headerData: headerData
-  })
+  }),
+  methods: {
+    moveTo(element) {
+        this.$scrollTo(element, 1100, {
+        easing: 'ease-in-out',
+        offset: -70
+    })
+  },
+  }
 }
 </script>
