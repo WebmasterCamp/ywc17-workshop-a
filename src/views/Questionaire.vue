1468cad2464aeec20d338014da940967febc23b1
Nutchy <nutchy@loomx.io>
2019-12-28 10:59:06 +0700

    init questionaire

--- src/views/Questionaire.vue
+++ src/views/Questionaire.vue
@@ -1,12 +1,28 @@
 <template>
     <b-container>
-        questionair page
+       <div v-for="(q, i) in loveQuestions" :key="i">
+           <div class="d-block">{{q.question}}</div>
+           <b-row>
+               <b-col cols="3">{{q.answers}}</b-col>
+           </b-row>
+       </div>
     </b-container>
 </template>
 
 <script>
 import Vue from 'vue'
-export default {
-    
+import { Component } from 'vue-property-decorator'
+import Questionaire from '@/mock_data/questionaire.json'
+
+@Component({
+
+})
+export default class QuestionairePage extends Vue {
+    get loveQuestions() {
+        return Questionaire.love
+    }
+    mounted() {
+        console.log(Questionaire)
+    }
 }
 </script>
