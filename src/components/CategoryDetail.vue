a710430ee95752407536480279b74eae854d65e0
Nutchy <nutchy@loomx.io>
2019-12-28 10:22:54 +0700

    remove default css

---
+++ src/components/CategoryDetail.vue
@@ -0,0 +1,11 @@
+<template>
+    <b-container>
+        <b-row>
+            <b-col cols="8">Icon</b-col>
+            <b-col> content</b-col>
+        </b-row>
+    </b-container>
+</template>
+<style>
+
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,7 +2,15 @@
   <div class="home">
    <b-container>
      test
+
+
    </b-container>
+    <CategoryDetail></CategoryDetail>
+    <CategoryDetail></CategoryDetail>
+    <CategoryDetail></CategoryDetail>
+    <CategoryDetail></CategoryDetail>
+
+
 
   </div>
 </template>
@@ -10,11 +18,12 @@
 <script>
 // @ is an alias to /src
 import HelloWorld from '@/components/HelloWorld.vue'
+import CategoryDetail from '@/components/CategoryDetail.vue'
 
 export default {
   name: 'home',
   components: {
-    HelloWorld
+    HelloWorld, CategoryDetail
   }
 }
 </script>
